fc469ae785a078fc32252d7a09e4b8b2c76d304f
chenxiyao <chenxiyao@emay.cn>
2018-11-02 13:55:16 +0800

    去除黄叹号

--- src/test/java/cn/emay/utils/regular/TestRegularUtils.java
+++ src/test/java/cn/emay/utils/regular/TestRegularUtils.java
@@ -1,22 +1,16 @@
 package cn.emay.utils.regular;
 
-import org.junit.Test;
-
-import cn.emay.utils.regular.RegularUtils;
-
 public class TestRegularUtils {
 
-
 	public static void main(String[] args) {
-		boolean flag=RegularUtils.matches("^\\d$", "1");
-		String str=RegularUtils.specialCodeEscape("#@$$%^^&&&*(())!");
-		boolean flag2=RegularUtils.checkString("zhong中文");
-		boolean flag3= RegularUtils.notExistSpecial("hanyou特殊字符￥@@");
-		System.out.println("是否匹配检查："+flag);
-		System.out.println("特殊字符转义："+str);
-		System.out.println("验证只含有中文和英文："+flag2);
-		System.out.println("校验是否包含特殊字符："+flag3);
+		boolean flag = RegularUtils.matches("^\\d$", "1");
+		String str = RegularUtils.specialCodeEscape("#@$$%^^&&&*(())!");
+		boolean flag2 = RegularUtils.checkString("zhong中文");
+		boolean flag3 = RegularUtils.notExistSpecial("hanyou特殊字符￥@@");
+		System.out.println("是否匹配检查：" + flag);
+		System.out.println("特殊字符转义：" + str);
+		System.out.println("验证只含有中文和英文：" + flag2);
+		System.out.println("校验是否包含特殊字符：" + flag3);
 	}
-
 
 }
