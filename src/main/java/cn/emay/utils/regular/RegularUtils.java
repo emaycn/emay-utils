e461e64ef699aa715f590946f58f1e4fc14fec3b
changyiwen <changyiwen@eamy.cn>
2018-11-02 11:34:56 +0800

    正则工具类增加三个方法

--- src/main/java/cn/emay/utils/regular/RegularUtils.java
+++ src/main/java/cn/emay/utils/regular/RegularUtils.java
@@ -27,4 +27,50 @@ public class RegularUtils {
 		return m.matches();
 	}
 
+	/**
+	 *
+	 * @Title: specialCodeEscape
+	 * @Description: 特殊字符转义
+	 * @param @param value
+	 * @param @return
+	 * @return String
+	 */
+	public static String specialCodeEscape(String value) {
+		if (null != value && !"".equals(value)) {
+			value = value.replace("%", "\\%").replace("_", "\\_");
+		}
+		return value;
+	}
+
+
+	/**
+	 * 验证只含有中文和英文
+	 *
+	 * @param val
+	 * @return
+	 */
+	public static boolean checkString(String val) {
+		if (null == val || "".equals(val)) {
+			return false;
+		}
+		String regExp = "^[\u2E80-\u9FFFa-zA-Z]+$";
+		Pattern p = Pattern.compile(regExp);
+		Matcher m = p.matcher(val);
+		return m.matches();
+	}
+
+
+	/**
+	 * 校验是否包含特殊字符
+	 *
+	 * @return false 包含
+	 * @return true 不包含
+	 */
+	public static boolean notExistSpecial(String value) {
+		String regExp = "[\u4e00-\u9fa5\\w]+";
+		Pattern p = Pattern.compile(regExp);
+		Matcher m = p.matcher(value);
+		return m.matches();
+	}
+
 }
--- src/test/java/cn/emay/utils/regular/TestRegularUtils.java
+++ src/test/java/cn/emay/utils/regular/TestRegularUtils.java
@@ -6,9 +6,17 @@ import cn.emay.utils.regular.RegularUtils;
 
 public class TestRegularUtils {
 
-	@Test
-	public void test() {
-		RegularUtils.matches("^\\d$", "1");
+
+	public static void main(String[] args) {
+		boolean flag=RegularUtils.matches("^\\d$", "1");
+		String str=RegularUtils.specialCodeEscape("#@$$%^^&&&*(())!");
+		boolean flag2=RegularUtils.checkString("zhong中文");
+		boolean flag3= RegularUtils.notExistSpecial("hanyou特殊字符￥@@");
+		System.out.println("是否匹配检查："+flag);
+		System.out.println("特殊字符转义："+str);
+		System.out.println("验证只含有中文和英文："+flag2);
+		System.out.println("校验是否包含特殊字符："+flag3);
 	}
+
 
 }
